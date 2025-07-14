75b1330fc05aedf5bfc66bd7deaf404f812c3f93
Santt4na <jhonnsantt4na@gmail.com>
2025-07-14 19:12:34 -0300

    Fix(VERSION CONFLICT): Resolve version conflict and not compilation of the code

--- flowstore-order/src/main/java/com/santt4na/flowstore_order/mapper/OrderMapper.java
+++ flowstore-order/src/main/java/com/santt4na/flowstore_order/mapper/OrderMapper.java
@@ -6,22 +6,18 @@ import com.santt4na.flowstore_order.entity.Order;
 import com.santt4na.flowstore_order.entity.OrderItem;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import org.mapstruct.factory.Mappers;
 
 import java.util.Set;
 
 @Mapper(componentModel = "spring")
 public interface OrderMapper {
 	
-	@Mapping(target = "items", source = "items")
 	OrderDTO toDto(Order order);
 	
-	@Mapping(target = "productId", source = "")
-	@Mapping(target = "id.productId", source = "productId")
+	@Mapping(target = "productId", source = "id.productId")
 	OrderItemDTO toDto(OrderItem orderItem);
 	
 	Set<OrderItemDTO> mapItems(Set<OrderItem> items);
-	
 	
 	Order toEntity(OrderDTO dto);
 	OrderItem toEntity(OrderItemDTO dto);
