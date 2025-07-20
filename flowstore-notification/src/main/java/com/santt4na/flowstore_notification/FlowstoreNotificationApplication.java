65b362ba411a900784613092419f5ae17288cefc
Santt4na <jhonnsantt4na@gmail.com>
2025-07-20 09:27:49 -0300

    feat(flowstore-notification) starting service the notification

---
+++ flowstore-notification/src/main/java/com/santt4na/flowstore_notification/FlowstoreNotificationApplication.java
@@ -0,0 +1,13 @@
+package com.santt4na.flowstore_notification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowstoreNotificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlowstoreNotificationApplication.class, args);
+	}
+
+}
